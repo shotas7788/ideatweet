b601558c47860b14bd7324e33a7cfeffc74a93da
shotas7788 <shotas7788@vivaldi.net>
2019-05-04 11:00:34 +0000

    changed application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,19 +12,19 @@ module Ideatweet
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     
-    config.time_zone = 'Tokyo'
-    config.i18n.default_locale = :ja
+  #   config.time_zone = 'Tokyo'
+  #   config.i18n.default_locale = :ja
   end
   
-  config.generators do |g|
-  g.test_framework :rspec,
-    fixtures: true,
-    view_specs: false,
-    helper_specs: false,
-    routing_specs: false,
-    controller_specs: true,
-    request_specs: false
-  g.fixture_replacement :factory_girl, dir: "spec/factories"
-  end
-
+  # config.generators do |g|
+  # g.test_framework :rspec,
+  #   fixtures: true,
+  #   view_specs: false,
+  #   helper_specs: false,
+  #   routing_specs: false,
+  #   controller_specs: true,
+  #   request_specs: false
+  # g.fixture_replacement :factory_girl, dir: "spec/factories"
+  # end
+  # rspecについては、後ほど設定
 end
